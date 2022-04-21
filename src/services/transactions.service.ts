1d9e9ffd668dcdc0502dbd3d7a86f2d97bf2bc20
aantoshaa_ <aapalanjuk@gmail.com>
2022-04-21 17:44:27 +0300

    fix

--- src/services/transactions.service.ts
+++ src/services/transactions.service.ts
@@ -2,14 +2,9 @@ import { AppDataSource } from "../config/db/appDataSource";
 import { User } from "../entities/user.entity";
 import { NoMoneyException } from "../error-handnling/transactions.exceptions";
 import { UserRepostirory } from "../repositories/user.repository";
-import { Operation } from "../shared/enums/operation.enums";
+import { Operation } from "../shared/enums/operation.enum";
 
-//TODO TEST THIS
 export class TransactionsSerivce {
-  private static async transactionsLogging() {
-    console.log("abc");
-  }
-
   private static async commonSumChanging(
     id: number,
     sum: number,
@@ -26,7 +21,6 @@ export class TransactionsSerivce {
 
     return result.raw;
   }
-
   static async increaceSum(id: number, sum: number) {
     return this.commonSumChanging(id, sum, Operation.INCREASE);
   }
@@ -41,14 +35,11 @@ export class TransactionsSerivce {
 
     await queryRunner.connect();
     await queryRunner.startTransaction();
-
     try {
       fromUser.sum -= Number(sum);
       await queryRunner.manager.save(fromUser);
-
       toUser.sum += Number(sum);
       await queryRunner.manager.save(toUser);
-
       await queryRunner.commitTransaction();
     } catch (err) {
       queryRunner.rollbackTransaction();
